7beea9b0d9429d8e1d8c16399319fcf094f2b9ac
AJITHRAJA8 <ajithrajar79@gmail.com>
2026-06-15 17:56:02 +0530

    add a password hash

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask,session
+from flask import Flask,session,request,redirect,render_template,url_for
 import mysql.connector
+from werkzeug.security import generate_password_hash,check_password_hash
 
 app=Flask(__name__)
 
@@ -16,6 +17,26 @@ else:
     print("Database Connection Fails")
 
 #login Page
+@app.route('/',methods=['GET','POST'])
+def login():
+    if request.method=='POST':
+        username=request.form['username']
+        password=request.form['password']
+        hash_password = generate_password_hash(password)
+        res=con.cursor()
+        sql='select * from login where username=%s and password=%s'
+        value=(username,hash_password)
+        res.execute(sql,value)
+        user=res.fetchone()
+
+        #Login functions
+        if user and check_password_hash(user['password'],password):
+            session[user] = username
+            return redirect(url_for('home'))
+        else:
+            return 'Invaild username or password'
+    return render_template('login.html')
+
 
 if __name__=="__main__":
     app.secert_key="Ajith123"
